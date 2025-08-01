ff5ac8c53f23afeae5cea7a0f67cc19c43d13341
OmolloRovy <masimevanistelrooy@gmail.com>
2025-08-01 19:31:47 +0300

    debugging 1.0

--- models/User.js
+++ models/User.js
@@ -9,6 +9,6 @@ const userSchema = new mongoose.Schema({
 },{minimize: false })
 
 
-const User =mongoose.models.user || mongoose.model('user', userSchema)
+const User = mongoose.models.user || mongoose.model('user', userSchema);
 
-export default mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
