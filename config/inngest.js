d782f79421a9ea6f50cdeba980981fc236b4b471
OmolloRovy <masimevanistelrooy@gmail.com>
2025-08-01 05:28:52 +0300

    inngest rename corr

--- config/inngest.js
+++ config/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
-import User from "@/models/User";
-import next from 'next';
+import User from "../../models/User";
+
 
 
 // Create a client to send and receive events
